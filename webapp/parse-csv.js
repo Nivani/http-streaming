435c130e5d358e26fd2b40b9549ac8585da5cf98
Nick Van Nieuwenhuyse <nick.van.nieuwenhuyse@gmail.com>
2022-01-21 12:14:41 +0100

    reduce nesting in readLines()

--- webapp/parse-csv.js
+++ webapp/parse-csv.js
@@ -21,16 +21,14 @@ async function *readLines(reader) {
     for await (const chunk of readChunks(reader)) {
         const chunkText = textDecoder.decode(chunk);
         const chunkLines = chunkText.split('\n');
-        if (chunkLines.length > 0) {
-            if (chunkLines.length === 1) {
-                partOfLine += chunkLines[0];
-            } else {
-                yield partOfLine + chunkLines[0];
-                for (let i=1; i < chunkLines.length - 1; i++) {
-                    yield chunkLines[i];
-                }
-                partOfLine = chunkLines[chunkLines.length - 1];
+        if (chunkLines.length === 1) {
+            partOfLine += chunkLines[0];
+        } else if (chunkLines.length > 1) {
+            yield partOfLine + chunkLines[0];
+            for (let i=1; i < chunkLines.length - 1; i++) {
+                yield chunkLines[i];
             }
+            partOfLine = chunkLines[chunkLines.length - 1];
         }
     }
 }
